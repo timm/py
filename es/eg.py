a6ed4b41b9383d8014179ece4a9c262f2e093196
Tim Menzies <timm@ieee.org>
2021-03-06 01:41:53 -0500

    saving

--- es/eg.py
+++ es/eg.py
@@ -1,6 +1,18 @@
 #!/usr/bin/env python3
 import sys
-from es import Tab,csv,THE
+from es import Tab,csv,THE,Counts
 
 t= Tab().adds(csv("data/auto93.csv"))
-a, b = t.extremes(THE)
+a, b = t.poles(THE)
+a = t.clone(a)
+b = t.clone(b)
+print(a.ys())
+print(b.ys())
+
+
+t= Tab().adds(csv("data/auto93.csv"))
+Counts(THE,t)
+
+
+
+
--- es/es.py
+++ es/es.py
@@ -36,27 +36,46 @@ def csv(file):
     for line in fp: 
       line = re.sub(r'([\n\t\r ]|#.*)','',line)
       if line:
-         yield [atom(x) for x in line.split(",")]
+        yield [atom(x) for x in line.split(",")]
 
+def Span(down=LO, up=HI):
+  i = it(down=down, up=up)
+  def has(i,x): 
+    return (x==i.down) if (i.down==i.up) else (i.down <= x < i.up)
+  return i + locals()
+ 
 def Num(pos=0,txt=""):
-  i = it(n=0, pos=pos, txt=txt, mu=0, m2=0, sd=0, 
+  i = it(n=0, pos=pos, txt=txt, _all=[], ok=False,
          lo=HI, hi=LO, w= -1 if LESS in txt else 1)
   def add(i,x):
     if x != NO:
       i.n    += 1
-      delta   = x - i.mu
-      i.mu   += delta/i.n
-      i.m2   += delta*(x - i.mu)
-      i.sd    = (i.m2/i.n)**0.5
+      i._all += [x]
+      i.ok    = False
       i.hi    = max(i.hi,x)
       i.lo    = min(i.lo,x)
     return x
-  def like(i,x, *l):
-    var   = i.sd**2
-    denom = (math.pi*2*var)**.5
-    num   = math.e**(-(x-i.mu)^2/(2*var+0.0001))
-    return num/(denom + 10^-64)
-  def norm(i,x): return (x - i.lo) / (i.hi - i.lo +10**-64)
+  def all(i):
+    i._all = i._all if i.ok else sorted(i._all)
+    i.ok = True
+    return i._all
+  def mid(i):    a=i.all(); return a[int(len(a)/2)]
+  def norm(i,x): a=i.all(); return (x-a[0])/(a[-1] - a[0])
+  def sd(i):     a=i.all(); return (a[int(.9*len(a))] - a[int(.1*len(a))])/2.56
+  def spans(i,j):
+    "https://stackoverflow.com/questions/41368653/intersection-between-gaussian"
+    m1,m2 = i.mid(), j.mid()
+    s1,s2 = i.sd(),  j.sd()
+    if s1==s2:
+      mid = (m1+m2)/2
+    else:
+      a  = (s1**2) - (s2**2)
+      b  = 2 * (m1 * s2**2 - m2 * s1**2)
+      c  = m2**2 * s1**2 - m1**2 * s2**2 - 2 * s1**2 * s2**2 * math.log(s1/s2)
+      x1 = (-b + math.sqrt(b**2 - 4 * a * c)) / (2 * a)
+      x2 = (-b - math.sqrt(b**2 - 4 * a * c)) / (2 * a)
+      mid = x1 if m1 <= x1 <= m2 else x2
+    return [Span(up=mid), Span(down=mid)] 
   return i + locals()
 
 def Sym(pos=0,txt=""):
@@ -68,27 +87,31 @@ def Sym(pos=0,txt=""):
       if tmp > i.most:
         i.most, i.mode = tmp, x
     return x
-  def like(i,x, THE, prior, ns):
-    return (i.seen.get(x,0) + THE.m*prior)/ (ns + THE.m)
+  def mid(i): return i.mode
+  def spans(i,j): 
+    return [Span(k,k) for k in (i.seen | j.seen)]
   return i + locals()
 
 def Skip(pos=0, txt=""):
   i = it(pos=pos, txt=txt, n=0)
   def add(i,x):
-    if x != NO: i.n    += 1
+    if x != NO: i.n += 1
     return x
   return i + locals()
 
 def Cols():
   i = it(all=[], y=[], x=[], klass=None, head=[]) 
-  def add(i,pos,txt):
+  def adds(i, lst): 
+    return [col.add(lst[col.pos]) for col in i.all]  
+  def cols(i, lst):
+    i.all = [i.col(n,x) for n,x in enumerate(lst)]
+    return i.all
+  def col(i,pos,txt):
     nump  = lambda x: LESS  in x or MORE in x   or x[0].isupper()
     goalp = lambda x: KLASS in x or LESS in txt or MORE in txt
-    also  = ([]   if NO in txt else (i.y if goalp(txt) else i.x))
-    k     = (Skip if NO in txt else (Num if nump(txt)  else Sym))
-    z     = k(pos,txt) 
+    z     = (Skip if NO in txt else (Num if nump(txt)  else Sym))(pos,txt)
     if KLASS in txt: i.klass = z
-    also += [z]
+    ([] if NO in txt else (i.y if goalp(txt) else i.x)).append(z)
     i.head += [txt]
     return z
   return i + locals()
@@ -97,8 +120,8 @@ def Tab():
   i = it(rows=[], cols=Cols())
   def adds(i,src): 
     for lst in src:
-      if i.cols.all: i.rows += [[col.add(lst[col.pos]) for col in i.cols.all]]
-      else         : i.cols.all = [i.cols.add(n,x) for n,x in enumerate(lst)]
+      if    i.cols.all: i.rows += [i.cols.adds(lst)]
+      else: i.cols.all = i.cols.cols(lst)
     return i
   def better(i,r1,r2):
     s1,s2,n = 0,0,len(i.cols.y)
@@ -109,23 +132,38 @@ def Tab():
       s1   -= math.e**(w*(a-b)/n)
       s2   -= math.e**(w*(b-a)/n)
     return s1/n < s2/n
-  def extremes(i,THE): 
-    def gt(a,b): return 0 if id(a)==id(b) else (-1 if i.better(a,b) else 1)
-    rows =  sorted(i.rows, key=functools.cmp_to_key(gt))
-    best = min(THE.best,len(rows)//2)
-    return rows[:best], rows[-best:]
+  def poles(i,THE): 
+    gt   = lambda a,b: 0 if id(a)==id(b) else (1 if i.better(a,b) else -1)
+    i.rows = sorted(i.rows, key=functools.cmp_to_key(gt))
+    best = min(THE.best,len(i.rows)//2)
+    return i.rows[:best], i.rows[-best:]
+  def clone(i,inits=[]):
+    j = Tab()
+    j.cols.cols(i.cols.head)
+    j.adds(inits)
+    return j
+  def ys(i):
+    return [col.mid() for col in i.cols.y]
   return i + locals()
 
-
-def ints(m1, m2, s1, s2):
-    # coefficients of quadratic equation ax^2 + bx + c = 0
-    if s1==s2: return (m1+m2)/2
-    a = (s1**2.0) - (s2**2.0)
-    b = 2 * (m1 * s2**2.0 - m2 * s1**2.0)
-    c = m2**2.0 * s1**2.0 - m1**2.0 * s2**2.0 - 2 * s1**2.0 * s2**2.0 * math.log(s1/s2)
-    x1 = (-b + math.sqrt(b**2.0 - 4.0 * a * c)) / (2.0 * a)
-    x2 = (-b - math.sqrt(b**2.0 - 4.0 * a * c)) / (2.0 * a)
-    return x1, x2
-print(ints(5.28, 8.45, 0.91, 1.36))
-print(ints(10, 12, 1,1.1))
-
+def Counts(THE,tab):
+  i = it(counts={}, klasses={}, totals={})
+  def count(tab1, label):
+    def bin(x,bins):
+      if x != NO:
+        for b in bins:
+          if b.has(x): return b
+    def inc(d,k): d[k] = d.get(k,0) + 1
+    for row in rows:
+      if b := bin(row[col.pos], bins):
+        inc(out.klasses, label)
+        inc(out.totals,  col.pos)
+        inc(out.counts,  (label, col.txt, b.down, b.up))
+  ############################
+  nth,sth = tab.poles(THE)
+  nth,sth = tab.clone(nth), tab.clone(sth)
+  for nthcol,sthcol, in zip(nth.cols.x, sth.cols.x):
+    spans = nthcol.spans(sthcol)
+    count(nth,"best")
+    count(sth,"rest")
+    return out
