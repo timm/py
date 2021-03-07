6b2fed45d4d78eff350c274aed0459402da3d47b
Tim Menzies <timm@ieee.org>
2021-03-07 12:19:09 -0500

    saving

--- es/visited.py
+++ es/visited.py
@@ -4,15 +4,14 @@ many things that you might think to test, wont ever happen
 of the things that do happen, a happen happen a lot and the others much less o
 """
 import random,sys
-
+import numpy as np
 from sparklines import sparklines
 
 random.seed( sys.argv[1] if len(sys.argv)>1  else 1)
 
 def v(a,n):
-  x=[100]
-  a=random.random()*a
-  for i in range(n-1): x += [int(x[-1]*a)]
+  x=[1]
+  for i in range(n-1): x += [x[-1]*a]
   return x
 
 def twod(m,alpha,bins):
@@ -29,9 +28,18 @@ def fived(m,alpha,bins):
   return [x*y*z*w*v for x  in m[0] for y in m[1] 
           for z in m[2] for w in m[3] for v in m[4]]
 
+def flatten(m, alpha, bins):
+  m = np.array(m)
+  a = np.array([1]*m.shape[1])
+  for d in range(m.shape[0]):
+       a = np.multiply(a, m[d])
+  return a
+
 def visited(gen,d=2,alpha=.9, bins=16,keep=10000):
   m      = [v(alpha, bins) for _ in range(d)]
-  a      = gen(m,alpha,bins)
+  a      = flatten(m,alpha,bins)
+  #print("\nflatten",list(sorted(a,reverse=True)))
+  a=gen(m,alpha,bins)
   s      = sum(a)
   kept   = [x for x in a if int(keep*x/s) > 0]
   visited= sorted([int(100*x/sum(kept)) for x in kept], reverse=True)
@@ -39,12 +47,10 @@ def visited(gen,d=2,alpha=.9, bins=16,keep=10000):
   print(f"at alpha={alpha}, {len(m)} vars visit {len(kept):>4} = {int(100*len(kept)/ len(a)):>3} % states at p>s{1/keep}",end="")
   print(". Visited:", sparklines(visited)[0])
 
-keep=100000
+keep=10000
 for alpha in [.3,.5,.7,.9]:
-  for _ in range(3):
     visited(twod,d=2,keep=keep,alpha=alpha, bins=10)
     visited(threed,d=3,keep=keep,alpha=alpha, bins=10)
     visited(fourd,d=4,keep=keep,alpha=alpha, bins=10)
     visited(fived,d=5,keep=keep,alpha=alpha, bins=10)
-
 
