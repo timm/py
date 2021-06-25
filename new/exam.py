9cce96d0b705765b7d40aeb46d5584ae93521a97
Tim Menzies <timm@ieee.org>
2021-06-25 15:17:19 -0400

    saving

--- new/exam.py
+++ new/exam.py
@@ -16,14 +16,21 @@ options = dict(
   data    = "../data/auto93.csv",
   do      = "none",
   p       = 2,
+  sample  = 32,
   seed    = 10013)
 
 import re,sys,copy,random,traceback
 from etc import o,csv,cli
 
+class  Ignore(o): 
+  def __init__(i,at,w): i.at=at
+  def add(i,z): return z
+
 class Num(o): 
-  def __init__(i): i._all, i.n, i.sorted = [],0,True
-  def add(i,z): i._all+=[z]; i.n += 1; i.sorted=False
+  def __init__(i,at,w):
+    i.at,i.w,i._all, i.n, i.sorted = at,w,txt,[],0,True
+  def add(i,z): 
+    z=float(z); i._all+=[z]; i.n += 1; i.sorted=False; return z
   def norm(i,z): 
     lo,hi = i.all()[0], i.all()[-1]
     return z  if z=="?" else (z-lo)/(hi - lo +1E-32)
@@ -31,55 +38,52 @@ class Num(o):
     if not i.sorted: i._all.sort()
     i.sorted=True
     return i._all
+  def dist(i,x,y):
+    if   x=="?": y = i.norm(y); x= 0 if y>.5 else 1
+    elif y=="?": x = i.norm(x); y= 0 if x>.5 else 1
+    else        : x, y = i.norm(x), i.norm(y)
+    return abs(x-y)
 
 def Sym(o): 
-  def __init__(i): i.seen, i.n = {},0
-  def add(i,z)   : i.seen[z] = 1 + i.seen.get(z,0); i.n += 1
-
+  def __init__(i,at,w): i.at,i.seen, i.n = at, {},0
+  def add(i,z): 
+    i.seen[z] = 1 + i.seen.get(z,0); i.n += 1; return z
+  def dist(i,x,y):
+    return 0 if x==y else 1
+    
 class Tab(o):
   def __init__(i,rows=[]):
     i.syms ={}; i.nums={}; i.rows=[] 
-    i.cols = o(txt=[], xs={}, ys={}, w={})
+    i.cols = o(header=[], xs=[], ys=[],all=[])
     for lst in rows: i.row(lst)
 
-  def clone(i,rows=[]): return Tab([i.cols.txt] + rows)
-  def slurp(i,f)      : [row(i,lst) for lst in csv(f)]
+  def clone(i,rows=[]): return Tab([i.cols.header] + rows)
+  def slurp(i,f)      : [i.row(lst) for lst in csv(f)]
   def row(i,lst)      : i.data(lst) if i.cols.txt else i.head(lst)
 
   def head(i,lst):
-    i.cols.txt = lst
+    i.cols.header = lst
     for c,txt in enumerate(lst):
-      if "?" not in s:
-        if txt[0].isupper():
-          i.nums[c] = Num()
-          i.cols.w[c] = -1 if "-" in txt else 1
-        else:
-          i.syms[c] = Sym()
+      weight = -1  if "-" in txt else 1
+      one = Ignore if "?" in s else (
+            Num if txt[0].isupper() else
+            Sym)
+      one = one(c, weight)
+      i.all += [one]
+      if type(what)==  Num: i.nums += [one]
+      if type(what)==  Sym: i.sym += [one]
   
   def data(i,lst):
-    for c in i.syms:
-      z = lst[c]
-      if z !="?": i.syms[c].add(z)
-    for c in i.nums:
-      z = lst[c]
-      if z !="?":
-        z = lst[c] = float(z)
-        i.nums[c].add(z)
+    for col in i.cols.all:
+      z = lst[col.at]
+      if z !="?": 
+        lst[c] = col.add(z)
     i.rows += [lst]
   
   def dist(i,r1,r2,the,cols=None):
-    cols = cols or i.cols.xs
-    def dist1(z1,z2,c):
-      if z1=="?" and z2=="?": return 1
-      if c in i.syms      : return 0 if z1==z2 else 1
-      if   z1=="?": z2 = i.norm(z2,c); z1= 0 if z2>.5 else 1
-      elif z2=="?": z1 = i.norm(z1,c); z2= 0 if z1>.5 else 1
-      else        : z1, z2 = norm(t,z1,c), norm(t,z2,c)
-      return abs(z1-z2)
-    #--------------
     gap,n = 0, 1E-32
-    for c in cols:
-      tmp  = dist1(r1[c], r2[c],c)
+    for col in cols or i.cols.xs:
+      tmp  = col.dist(r1[col.at], r2[col.at])
       gap += tmp**the.p
       n   += 1
     return (gap/n)**(1/the.p)  
@@ -87,7 +91,7 @@ class Tab(o):
   def div(i,the,rows=None,cols=None):
     def gap(z1,z2): return i.dist(z1, z2, the, cols)
     def far(r1):
-      tmp = [random.choice(rows or i.rows) for _ in range(32)]
+      tmp = [random.choice(rows or i.rows) for _ in range(the.sample)]
       tmp = sorted([(gap(r1,r2),r2) for r2 in tmp], key=lambda z:z[0])
       return tmp[-2]
     #-------------------------
