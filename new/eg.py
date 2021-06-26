89963926cb3bad76f7a55d2dd256d4c05dfb416a
Tim Menzies <timm@ieee.org>
2021-06-25 23:08:34 -0400

    save

--- new/eg.py
+++ new/eg.py
@@ -1,14 +1,15 @@
+#!/usr/bin/env python3.9
+
 import sys, copy, random, traceback
 from es import __doc__ as doc
-from es import options,Head,Data,slurp 
+from es import options,Tab,sway,cluster
 from etc import csv,cli,red,green
 
 def all(funs,defaults,help):
   fails = 0
   the = cli(defaults,help)
   for s, fun in funs.items():
-    if s[:2] != "eg": continue
-    if s[:2] == "eg" and (the.do=="all" or the.do==s[2:]):
+    if s[:2]=="eg" and (the.do=="all" or the.do==s[2:]):
       try:
         random.seed(the.seed)
         fun(copy.deepcopy(the))
@@ -21,16 +22,62 @@ def all(funs,defaults,help):
 
 def egfail(the): assert False, "a fail"
 
+def egnum(the): 
+  n=Num([1,2,3,4,5,6,7,8,9])
+
 def egshow(the): print(the)
 
 def eglines(the):
   [print(row) for row in csv(the.data)]
 
 def egtab(the):
-  h,d=Head(), Data()
-  slurp(the.data,h,d)
-  print(h)
-  print(d.lo)
+  t= Tab(csv("../data/auto93.csv"))
+  assert 5140 == t.y[0].all()[-1]
+  assert  398 == t.y[0].n
+  rows = sorted(t.rows)
+  lo  = t.clone(rows[:50]).goals()
+  hi  = t.clone(rows[50:]).goals()
+  print(lo,"<== lo")
+  print(hi,"<== hi")
+  print([("+++" if c.w>0 else "---") for c in t.y])
+  print([c.w for c in t.y])
+  print([col.txt for col in t.y])
+
+def egtabChina(the):
+  t= Tab(csv("../data/china.csv"))
+  rows = sorted(t.rows)
+  lo  = t.clone(rows[:50]).goals()
+  hi  = t.clone(rows[50:]).goals()
+  print(lo,"<== lo")
+  print(hi,"<== hi")
+  print([("+++" if c.w>0 else "---") for c in t.y])
+  print([c.w for c in t.y])
+  print([col.txt for col in t.y])
+
+def egdist(the):
+  t = Tab(csv(the.data)) 
+  for row1 in t.rows[:5]:
+    for row2 in t.rows[5:10]:
+      print(row1.dist(row2,the))
+
+def egcluster(the):
+  t = Tab(csv(the.data)) 
+  for t1 in sorted(cluster(t,the, t.y)):
+    print(t1.goals())
+  print([("+++" if c.w>0 else "---") for c in t.y])
+
+def egsway(the):
+  r = lambda lst: [round(x,3) for x in lst]
+  t = Tab(csv(the.data)) 
+  rows = sorted(t.rows)
+  lo  = t.clone(rows[:25]).goals()
+  hi  = t.clone(rows[25:]).goals()
+  print(sway(t,the, t.x).goals(),"<== sway")
+  print(lo,"<== lo")
+  print(hi,"<== hi")
+  print([("+++" if c.w>0 else "---") for c in t.y])
+  print([c.w for c in t.y])
+  print(r([col.var()*.35 for col in t.y]))
 
 if __name__ == "__main__":
   all(vars(), options, doc)
--- new/es.py
+++ new/es.py
@@ -10,13 +10,14 @@ Options:
  -p       INT    distance co-efficient; default=2
  -seed    INT    default random number seed; default=10013
 """
-import re,sys,copy,random,traceback
-from etc import o,csv,cli
+import re, sys, copy, math, random, traceback
+from etc import o, csv, cli
 
 options = dict(
   Verbose = 0,
   data    = "../data/auto93.csv",
   do      = "none",
+  enough  = .5,
   l       = 2,
   sample  = 32,
   seed    = 10013)
@@ -26,6 +27,7 @@ class Col(o):
     i.n, i.txt, i.at, i.w = 0,txt, at, -1 if "-" in txt else 1
     [self.add(z) for z in inits]
   def add(i,z): return z
+  def mid(i)  : return "?"
   def dist(i,x,y): 
     return 1 if x=="?" and y=="?" else i.dist1(x,y)
 
@@ -36,11 +38,15 @@ class Num(Col):
     i._all, i.sorted = [],True
     super().__init__(*l, **kw)
   def add(i,z): 
-    z=float(z); i._all+=[z]; i.n += 1; i.sorted=False; return z
+    if z != "?":
+      i.n += 1
+      z=float(z); i._all+=[z]; i.sorted=False
+    return z
   def norm(i,z): 
     if z=="?": return z
     lo,hi = i.all()[0], i.all()[-1]
-    return (z-lo)/(hi - lo +1E-32)
+    tmp = (z-lo)/(hi - lo +1E-32)
+    return  max(0,min(tmp,1))
   def all(i):
     if not i.sorted: i._all.sort()
     i.sorted=True
@@ -50,52 +56,96 @@ class Num(Col):
     elif y=="?": x = i.norm(x); y= 0 if x>.5 else 1
     else       : x, y = i.norm(x), i.norm(y)
     return abs(x-y)
+  def per(i,p=.5): 
+    a=i.all()
+    return a[int(p*len(a))]
+  def mid(i): return i.per(.5)
+  def var(i): return (i.per(.9) - i.per(.1))/2.56
 
-def Sym(Col): 
+class Sym(Col): 
   def __init__(i,*l,**kw):
-    i.seen = {}
+    i.seen, i.mode, i.most = {},None,0
     super().__init__(*l, **kw)
-  def add(i,z): 
-    i.seen[z] = 1 + i.seen.get(z,0); i.n += 1; return z
-  def dist1(i,x,y):
-    return 0 if x==y else 1
-    
+  def add(i,z,n=1): 
+    if z != "?":
+      i.n += n 
+      tmp = i.seen[z] = i.seen.get(z,0) + n
+      if tmp > i.most:
+        i.most,i.mode = tmp,z
+    return z
+  def mid(i): return i.mode
+  def dist1(i,x,y): return 0 if x==y else 1
+   
+class Row(o):
+  def __init__(i,t, cells): i._tab,i.cells = t,cells
+
+  def __lt__(i, j):
+    cols = i._tab.y
+    s1, s2, n = 0, 0, len(cols)
+    for col in cols:
+      a,b = i.cells[col.at], j.cells[col.at]
+      if a=="?" or b=="?": continue
+      a,b = col.norm(a), col.norm(b)
+      s1 -= math.e**(col.w * (a - b) / n)
+      s2 -= math.e**(col.w * (b - a) / n)
+    return s1 / n < s2 / n
+
+  def dist(i,j,the,cols=None):
+    gap,n = 0, 1E-32
+    for col in cols or i._tab.x:
+      tmp  = col.dist(i.cells[col.at], j.cells[col.at])
+      gap += tmp**the.l
+      n   += 1
+    return (gap/n)**(1/the.l)  
+
 class Tab(o):
   def __init__(i,rows=[]):
     i.xy,i.x,i.y,i.rows = [],[],[],[]
     for lst in rows: i.row(lst)
 
-  def clone(i,rows=[]): return Tab([[col.txt for col in i.xy] + rows])
-  def fromFile(i,f)   : [i.row(lst) for lst in csv(f)]
-  def row(i,lst)      : (i.data if i.xy else i.head)(i,lst)
+  def __lt__(i,j): 
+    return Row(i, i.mid()) < Row(j,j.mid())
+
+  def goals(i): 
+    return [col.mid() for col in i.y]
+
+  def mid(i): 
+    return [col.mid() for col in i.xy]
+
+  def clone(i,rows=[]): 
+     t= Tab()
+     t.row([c.txt for c in i.xy])
+     [t.row(row) for row in rows]
+     return t
+
+  def row(i,lst)      : 
+    lst = lst.cells if type(lst)==Row else lst
+    (i.data if i.xy else i.head)(lst)
 
   def head(i,lst):
     for at,s in enumerate(lst):
       ako = Skip if "?" in s else (Num if s[0].isupper() else Sym)
-      one = ako(s,at)
+      one = ako(txt=s,at=at)
       i.xy += [one]
-      (i.y if "+" in s or "-" in s or "!" in s else i.x).append(one)
+      if type(one) != Skip:
+        if "+" in s or "-" in s or "!" in s:
+              i.y += [one]
+        else: i.x += [one]
   
   def data(i,lst):
     for col in i.xy:
       z = lst[col.at]
-      if z != "?": lst[c] = col.add(z)
-    i.rows += [lst]
+      if z != "?": 
+        lst[col.at] = col.add(z)
+    i.rows += [Row(i,lst)]
   
-  def dist(i,r1,r2,the,cols=None):
-    gap,n = 0, 1E-32
-    for col in cols or i.x:
-      tmp  = col.dist(r1[col.at], r2[col.at])
-      gap += tmp**the.l
-      n   += 1
-    return (gap/n)**(1/the.l)  
-
   def div(i,the,rows=None,cols=None):
-    def gap(z1,z2): return i.dist(z1, z2, the, cols)
+    def gap(z1,z2): 
+      return z1.dist(z2, the, cols)
     def far(r1):
-      tmp = [random.choice(rows or i.rows) for _ in range(the.sample)]
-      tmp = sorted([(gap(r1,r2),r2) for r2 in tmp], key=lambda z:z[0])
-      return tmp[-3]
+      a= [random.choice(rows or i.rows) for _ in range(the.sample)]
+      a= sorted([(gap(r1,r2),r2) for r2 in a], key=lambda z:z[0])
+      return a[-3][1]
     #-------------------------
     zero = random.choice(rows)
     one  = far(zero)
@@ -106,6 +156,36 @@ class Tab(o):
       a = gap(row, one)
       b = gap(row, two)
       tmp[id(row)] = (a**2 + c**2 - b**2) / (2 * c + 1E-31)
+    #print(tmp)
     rows.sort(key=lambda row: tmp[id(row)])
     mid = len(rows) // 2
-    return rows[:mid], rows[mid:]
+    return one, two, rows[:mid], rows[mid:]
+
+def cluster(t,the, cols=None):
+  def go(rows,lvl=0):
+    if type(lvl)==int:
+      print('|.. ' * lvl,len(rows))
+    if len(rows) < enough:
+      out.append(t.clone(rows))
+    else:
+      _, _, lefts, rights = t.div(the,rows=rows,cols=cols)
+      go(lefts,   lvl+1)
+      go(rights, lvl+1)
+
+  enough = 2*len(t.rows)**the.enough
+  cols = cols or t.x
+  out = []
+  go(t.rows)
+  return out
+
+def sway(t,the, cols=None):
+  def go(rows,lvl=0):
+    if type(lvl) == int  : print('|.. ' * lvl,len(rows))
+    if len(rows) < enough: return t.clone(rows)
+    else:
+      left, right, lefts, rights = t.div(the,rows=rows,cols=cols)
+      return  go(lefts if left < right else rights, lvl+1)
+
+  enough = 2*len(t.rows)**the.enough
+  cols = cols or t.x
+  return go(t.rows)
