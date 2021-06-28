a162bed65c47aab95866886e6d83c6454ff826f5
Tim Menzies <timm@ieee.org>
2021-06-28 13:41:40 -0400

    save

--- v2/etc.lisp
+++ v2/etc.lisp
@@ -4,22 +4,27 @@
 (defmacro whale (expr &body body) 
   `(do ((a ,expr ,expr)) ((not a)) ,@body))
 
+(defun num?(s0)
+  (let ((s1 (ignore-errors 
+              (with-input-from-string (in s0) (read in)))))
+    (if (typep s1 'number) s1 s0)))
+
+(defun blackspace(str) 
+  (remove-if #'(lambda (c) (member c '(#\space #\tab))) str))
+
 (defun str->words1 (str &optional (lo 0))
   (aif (position #\comma str :start lo)
-    (cons (subseq str lo  it) (str->words1 str (1+ it) ))
-    (list (subseq str lo))))
+       (cons (subseq str lo  it) (str->words1 str (1+ it) ))
+       (list (subseq str lo))))
 
 (defun str->words (str) 
-  (let ((str (remove-if #'(lambda (c) (member c '(#\space #\tab))) str)))
-    (if (not (zerop (length str))) 
-      (str->words1 str)))) 
+  (let ((str (blackspace str)))
+    (unless (zerop (length str)) (str->words1 str))))
 
 (defun file->words (f fn)
   (with-open-file (s f) 
     (whale (read-line s nil)
-           (aif (str->words a) (funcall fn it)))))
+           (funcall fn (mapcar #'num? (str->words a))))))
 
-(print (str->words ""))
-(print (str->words " "))
 
-(file->words "../data/auto93.csv" #'print)
+(file->words "../data/auto93.csv" #'identity)
